f2b6216054154c77e12b49c9013376fac431ff85
Keyur Bhalodiya | KB <kbit920@outlook.com>
2024-07-02 15:37:12 +0530

    Initial Commit

---
+++ CurrencyConversionApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CurrencyConversionApp
+//
+//  Created by Keyur Bhalodiya on 2024/07/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CurrencyConversionApp/CurrencyConversionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConversionAppApp.swift
+//  CurrencyConversionApp
+//
+//  Created by Keyur Bhalodiya on 2024/07/02.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyConversionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
