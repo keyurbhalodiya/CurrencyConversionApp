acb1249856fd87b0f50c53f4acfb2b8121542abe
Keyur Bhalodiya | KB <kbit920@outlook.com>
2024-07-05 10:32:02 +0530

    * Add Done button on keyboard tollbar to dismiss the keyboard

--- CurrencyConversionApp/View/CurrencyAmountTextField.swift
+++ CurrencyConversionApp/View/CurrencyAmountTextField.swift
@@ -12,6 +12,7 @@ struct CurrencyAmountTextField: View {
   @Binding var amount: Double?
   private let currencyCode: String
   private let tapHander: () -> Void
+  @FocusState private var isFocused: Bool
 
   init(amount: Binding<Double?>, currencyCode: String, tapHander: @escaping () -> Void) {
     self._amount = amount
@@ -37,6 +38,19 @@ struct CurrencyAmountTextField: View {
         .overlay(RoundedRectangle(cornerRadius: 5)
           .stroke(Color.gray)
         )
+        .focused($isFocused)
+        .toolbar {
+          if isFocused {
+            ToolbarItemGroup(placement: .keyboard) {
+                HStack {
+                    Spacer()
+                    Button(action: {
+                        isFocused = false
+                    }) { Text("Done") }
+                }
+            }
+          }
+        }
     }
     .foregroundColor(.black)
     .font(.system(size: 20, weight: .medium, design: .default))
